71864c9de19ab82304791f7f986e75acc669f2dd
kazuki2000 <pkazuki2000@icloud.com>
2023-07-26 16:42:50 +0900

    ファースト

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Admin, Resource } from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import { Dashboard } from "./Dashboard";
+import { authProvider } from "./authProvider";
+
+export const App = () => (
+  <Admin authProvider={authProvider} dataProvider={dataProvider} dashboard={Dashboard}>
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource
+      name="users"
+      list={UserList}
+      icon={UserIcon}
+      recordRepresentation="name"
+    />
+  </Admin>
+);
---
+++ src/users.tsx
@@ -0,0 +1,17 @@
+import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
+import MyUrlField from "./MyUrlField";
+
+export const UserList = () => {
+  return (
+    <List>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <TextField source="name" />
+        <EmailField source="email" />
+        <TextField source="phone" />
+        <MyUrlField source="website" />
+        <TextField source="company.name" />
+      </Datagrid>
+    </List>
+  );
+};
